8915fff19cab0d9ed0d2bf4a430bfb4d1b06d153
AlexGithub777 <111836904+AlexGithub777@users.noreply.github.com>
2022-10-09 15:24:37 +1300

    court pages started

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,7 +1,6 @@
 let courts = [{id: 0, name: "Marine Parade", place: "Napier", thumbs_up: 21, thumbs_down: 6, image_URL: "Images/marine_parade.jpeg", link_URL: "Courts/marine_parade.html"},
               {id: 1, name: "Fitzgerald Place Reserve", place: "Napier", thumbs_up: 18, thumbs_down: 6, image_URL: "Images/fitzgerald_place_reserve.jpg", link_URL: "Courts/fitzgerald_place_reserve.html"},
-              {id: 2, name: "Flaxmere Park", place: "Hastings", thumbs_up: 16, thumbs_down: 3, image_URL: "Images/flaxmere_park.jpg", link_URL: "Courts/flaxmere_park.html"},
-              {id: 3, name: "Wairoa Park", place: "Wairoa", thumbs_up: 16, thumbs_down: 3, image_URL: "Images/flaxmere_park.jpg", link_URL: "Courts/flaxmere_park.html"}]
+              {id: 2, name: "Flaxmere Park", place: "Hastings", thumbs_up: 16, thumbs_down: 3, image_URL: "Images/flaxmere_park.jpg", link_URL: "Courts/flaxmere_park.html"}]
             
 function display_courts() {
     let htmlContent = "";
@@ -25,6 +24,7 @@ function display_courts() {
     }
     document.getElementById("courtsBox").innerHTML = htmlContent;
 }
+
 
 
 
@@ -161,6 +161,90 @@ var TopRated= () => {
 
 document.getElementById("Top3RatedCourts").innerHTML = htmlContent;
 }
+
+
+function voting(court_like_num, court_dislike_num) {
+
+    let currentVotes = {like: court_like_num, dislike: court_dislike_num}
+    
+    //Variables to track the clicking status
+    //RULE: Allow to vote only one: UP or DOWN
+    let voteStatus = {like: false, dislike: false};
+
+    this.like = function () {
+        //Check current status of "like" button (has been clicked or not)
+        if (voteStatus.like == false) {
+            //Increase a "Like": Increase the like number by 1
+            document.getElementById("likeNumber").innerHTML = currentVotes.like + 1;
+            //Change the background color of Like button to GREEN
+            document.getElementById("likeButton").style.backgroundColor = "green";
+            //Change the current status of "like" button: has been clicked
+            voteStatus.like = true;//
+            
+            //Check "dislike" status - if dislike has been voted, down it by one & change status to False & change background color to white
+            if (voteStatus.dislike == true) {
+                document.getElementById("dislikeNumber").innerHTML = currentVotes.dislike;
+                voteStatus.dislike = false;//
+                document.getElementById("dislikeButton").style.backgroundColor = "white";
+            }		
+        } else {
+            //Keep the current number of like
+            document.getElementById("likeNumber").innerHTML = currentVotes.like;
+            //Change the background color of Like button to WHITE
+            document.getElementById("likeButton").style.backgroundColor = "white";
+            //Change the current status of "like" button
+            voteStatus.like = false;//has been clicked	
+        }	
+    }
+
+
+    //Click Like button
+   
+    
+    //Click Dislike button
+    this.dislike = function () {
+    //Check current status of "dislike" button (has been clicked or not)
+        if (voteStatus.dislike == false) {
+            //Increase a "disLike"  by 1
+            document.getElementById("dislikeNumber").innerHTML = currentVotes.dislike + 1;
+            //Change the background color of Like button to GREEN
+            document.getElementById("dislikeButton").style.backgroundColor = "red";
+            //Change the current status of "dislike" button
+            voteStatus.dislike = true;//has been clicked
+            
+            //Check "like" status - if like has been voted, down it by one & change status to False & change background color to white
+            if (voteStatus.like == true) {
+                document.getElementById("likeNumber").innerHTML = currentVotes.like;
+                voteStatus.like = false;//
+                document.getElementById("likeButton").style.backgroundColor = "white";
+            }
+            
+        } else {
+            //Keep the current number of of "dislike"
+            document.getElementById("dislikeNumber").innerHTML = currentVotes.dislike;
+            //Change the background color of disLike button to WHITE
+            document.getElementById("dislikeButton").style.backgroundColor = "white";
+            //Change the current status of "dislike" button
+            voteStatus.dislike = false;//has been clicked	
+        }	
+    }	
+}
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
     
 
 
@@ -185,4 +269,24 @@ window.onload = function() {
         display_courts();
         CHB();
     }
+
+    if (window.location.href.indexOf('marine_parade.html') > -1) {
+        court_like_num = document.getElementById("likeNumber").innerHTML = courts[0].thumbs_up;
+        court_dislike_num = document.getElementById("dislikeNumber").innerHTML = courts[0].thumbs_down;
+        voting(court_like_num, court_dislike_num);
+    }
+
+    if (window.location.href.indexOf('flaxmere_park.html') > -1) {
+        court_like_num = document.getElementById("likeNumber").innerHTML = courts[2].thumbs_up;
+        court_dislike_num = document.getElementById("dislikeNumber").innerHTML = courts[2].thumbs_down;
+        voting(court_like_num, court_dislike_num);
+    }
+
+    if (window.location.href.indexOf('fitzgerald_place_reserve.html') > -1) {
+        court_like_num = document.getElementById("likeNumber").innerHTML = courts[1].thumbs_up;
+        court_dislike_num = document.getElementById("dislikeNumber").innerHTML = courts[1].thumbs_down;
+        voting(court_like_num, court_dislike_num);
+    }
+
+    
   }
